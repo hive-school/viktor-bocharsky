4af7e9ad68dee1143e8e66879f99c021e712a038
Victor <bocharsky.bw@gmail.com>
2014-04-19 12:07:40 +0300

    Remove debug output

--- letter-counter.php
+++ letter-counter.php
@@ -2,6 +2,7 @@
 
 $string = 'Test string'; // test input string
 
+$string = stream_get_line(STDIN, 255, PHP_EOL); // Get string from console user input
 $letters = str_split($string); // split string to array by letters
 
 /* counting letter frequency with loop */
@@ -14,7 +15,8 @@ foreach ($letters as $letter) {
         $counter[$letter] = 1;
     }
 }
-unset($counter[' ']); // remove spaces
+unset($counter['']); // remove empty string
+unset($counter[' ']); // remove space
 arsort($counter, SORT_NUMERIC); // reverse sort by frequency
 
 /* printing letters with frequency */
