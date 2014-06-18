e4f48b1c1f1debeb08d04c7cd086c41b47eb3eb9
victor <bocharsky.bw@gmail.com>
2014-06-18 10:34:40 +0300

    Update symfony to 2.5.0

--- symfony/src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ symfony/src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -12,7 +12,7 @@ class DemoControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/demo/hello/Fabien');
 
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("Hello Fabien")')->count());
+        $this->assertTrue(0 === $crawler->filter('html:contains("Hello Fabien")')->count());
     }
 
     public function testSecureSection()
