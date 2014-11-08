f443387408bae1c3b176e8fc623df167be98f53b
Victor <bocharsky.bw@gmail.com>
2014-11-08 17:04:32 +0200

    Add project bundle with few entities

---
+++ php-symfony-1/src/BU/ProjectBundle/BUProjectBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BU\ProjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BUProjectBundle extends Bundle
+{
+}
---
+++ php-symfony-1/src/BU/ProjectBundle/Entity/Project.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace BU\ProjectBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Project
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Project
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="budget", type="integer")
+     */
+    private $budget;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Project
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Project
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set budget
+     *
+     * @param integer $budget
+     * @return Project
+     */
+    public function setBudget($budget)
+    {
+        $this->budget = $budget;
+
+        return $this;
+    }
+
+    /**
+     * Get budget
+     *
+     * @return integer 
+     */
+    public function getBudget()
+    {
+        return $this->budget;
+    }
+}
